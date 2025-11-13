793af8ef3cecc9a1d673d1925ef118cf52da10fc
jdj58 <jdj58@humboldt.edu>
2025-11-12 16:38:11 -0800

    Added Notes for Github
    We added brief notes about github and used ignore for : html and shp that were too large or no longer needed.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Misc
+# Misc #this is title
+
+##This is subtitle
+
 Misc workspace
+
+This is a Misc Workspace
+
+Mainly description of what the files mean
+
+list with - or a *
+
+#Git ignore is a list of files that wont be added to commits or changes.
+
+*.html ignores files you wont be using.
---
+++ calfire_data_T.R
@@ -0,0 +1,174 @@
+# cal fire data - potential plots for poster 
+
+# shapefile and timeseries 
+
+library(tidyverse)
+library(readr)
+library(lubridate)
+library(sf)
+
+setwd("C:/Users/Jonat/OneDrive/Documents/Improving Torres/Cal_Fire/")
+
+# load shapefile 
+ca_fires_shp <- st_read("California_Historic_Fire_Perimeters_-247493000743518952")
+
+# filter to Six Rivers NF 
+srf = ca_fires_shp %>% dplyr::filter(UNIT_ID=="SRF")
+
+# save shapefile 
+# write_sf(srf, "../six_rivers/srf_historic_fire_perimeters.shp")
+
+# plot for fires occurring after 2010 
+srf %>% dplyr::filter(YEAR_>2010) %>% 
+  ggplot(aes(fill=as.factor(CAUSE))) + 
+  geom_sf() 
+
+unique(srf$CAUSE)
+
+# match cause numbers to text 
+causes <- read.csv("fires data - Cause codes.csv") %>% 
+  rename(CAUSE=code, cause_text=X)
+
+srf_c <- left_join(srf, causes, by='CAUSE')
+
+# plot for fires occurring after 2010 
+srf_c %>% dplyr::filter(YEAR_>2010) %>% 
+  ggplot(aes(fill=cause_text)) + 
+  geom_sf() + 
+  scale_fill_brewer(palette='Set2') # changed color beccause rainbow is hard to read 
+
+##########################
+srf_c %>%
+  dplyr::filter(YEAR_ > 2010) %>%
+  ggplot(aes(fill = cause_text)) +
+  geom_sf() +
+  scale_fill_brewer(palette = 'Set2') +
+  #coord_sf(xlim = c(min_longitude, max_longitude)) +  # Replace with actual longitude bounds
+  labs(title = "Wildfire Causes After 2010",
+       fill = "Causes")
+#################################
+srf_c %>%
+  dplyr::filter(YEAR_ > 2010) %>%
+  ggplot(aes(fill = cause_text)) +
+  geom_sf() +
+  scale_fill_manual(values = c(
+    "darkgreen", "blue", "orange", "purple", "red",
+    "brown", "gray", "black", "gold", "navy", "magenta"
+  )) +
+  labs(
+    title = "Wildfire Causes After 2010",
+    fill = "Causes"
+  )
+
+
+
+###########################
+
+# other option - interactive
+# package leaflet 
+library(mapview)
+# plot 
+six_rivers = srf_c %>% dplyr::filter(YEAR_>2010)
+mapview(six_rivers, 
+        zcol = "cause_text")     
+
+
+# options to plot over a static basemap
+install.packages("ggmap")
+library(ggmap)
+
+# Source data - transform center of all points 
+xy <- st_coordinates(srf$geometry) %>% 
+  as.data.frame() %>% 
+  dplyr::select(X,Y) %>%
+  summarize_all(mean) %>% 
+  st_as_sf(coords=c("X","Y"), crs=3857) %>% 
+  st_transform((crs=4236))
+
+# get API for basemap - need credit card 
+register_google()
+
+base = get_map(location=c(lon=st_coordinates(xy)[1], 
+                          lat=st_coordinates(xy)[2]))
+
+# other option - static - basemap package 
+# package basemap 
+library(basemap)
+# Set the basemap extent based on your data
+basemap_ggplot(six_rivers) + geom_sf(data=six_rivers)
+
+# other option - static - ggspatial package 
+library(ggspatial)
+# plot like regular ggplot 
+ggplot() +
+  annotation_map_tile(type = "osm", zoomin = 0) +
+  geom_sf(data = six_rivers, aes(fill = cause_text), color = "grey", alpha = 0.9) +
+  coord_sf() +
+  #scale_fill_brewer(palette="Set2")
+  theme_minimal()
+
+
+## time series plots - causes over time 
+
+srf_c %>% as.data.frame %>% 
+  rename(year = YEAR_,
+         acres=GIS_ACRES) %>% 
+  ggplot(aes(x=year, y=acres, fill=cause_text)) +
+  geom_col() 
+#####################################################
+library(ggplot2)
+library(dplyr)
+
+srf_c %>%
+  as.data.frame() %>%
+  rename(year = YEAR_,
+         acres = GIS_ACRES) %>%
+  ggplot(aes(x = year, y = acres, fill = cause_text)) +
+  geom_col() +
+  scale_fill_manual(values = c("darkgreen", "blue", "orange", "purple", "red", "brown", "gray", "black", "gold", "navy", "magenta")
+) +  # Customize as needed
+  labs(
+    title = "Wildfire Causes Over Time",
+    x = "Year",
+    y = "Acres Burned",
+    fill = "Cause"
+  ) +
+  theme_minimal()
+
+#California Fire Perimetters CITATION
+
+######################################################
+# filtered years #Use this plot for causes
+srf_c %>% as.data.frame %>%
+  rename(year = YEAR_,
+         acres=GIS_ACRES, 
+         cause= cause_text) %>% 
+  filter(year>2000) %>% 
+  ggplot(aes(x=year, y=acres, fill=cause)) +
+  geom_col() +
+  scale_fill_brewer(palette="Set2")
+
+
+#######################################################
+# other option - static - ggspatial package 
+library(ggspatial)
+# plot like regular ggplot 
+ggplot() +
+  annotation_map_tile(type = "osm", zoom=10) +
+  geom_sf(data=sr_shp, fill="grey") + 
+  geom_sf(data = six_rivers, aes(fill = cause_text), color = "white", alpha = 0.9) +
+  coord_sf() +
+  #scale_fill_brewer(palette="Set2")
+  theme_minimal() +
+  # spatial-aware automagic scale bar
+  annotation_scale(location = "tl") +
+  # spatial-aware automagic north arrow
+  annotation_north_arrow(location = "br", which_north = "true")
+
+
+#Missing sr_shp
+
+
+
+
+#####################################################
